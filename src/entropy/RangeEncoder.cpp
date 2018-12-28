87ee8c55ac03015013bfb1ba89691b611142c487
Frederic Langlet <flanglet@gmail.com>
2018-12-27 17:05:33 -0800

    Improve encoding of distribution.

--- src/entropy/RangeEncoder.cpp
+++ src/entropy/RangeEncoder.cpp
@@ -66,7 +66,7 @@ bool RangeEncoder::encodeHeader(int alphabetSize, uint alphabet[], uint frequenc
         return true;
 
     _bitstream.writeBits(lr - 8, 3); // logRange
-    int inc = (alphabetSize > 64) ? 16 : 8;
+    int inc = (alphabetSize >= 64) ? 6 : 4;
     int llr = 3;
 
     while (1 << llr <= lr)
