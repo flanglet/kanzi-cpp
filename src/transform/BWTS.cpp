flanglet/kanzi-cpp

commit c075607e778bc140813439bce5c777c508b295d5

Stricter typing as a step in preparation for C++17 support (byte definition conflict).

src/transform/BWTS.cpp
@@ -145,7 +145,7 @@ int BWTS::moveLyndonWordHead(int sa[], int isa[], byte data[], int count, int st
         if ((k == size) && (rank < isa[nextStart]))
             break;
 
-        if ((k < size) && (nextStart < count) && ((data[start + k] & 0xFF) < (data[nextStart] & 0xFF)))
+        if ((k < size) && (nextStart < count) && ((data[start + k] & byte(0xFF)) < (data[nextStart] & byte(0xFF))))
             break;
 
         sa[rank] = nextStart0;

src/Memory.hpp
@@ -77,7 +77,7 @@ namespace kanzi {
 		#else
          static inline bool IS_BIG_ENDIAN() {
             union { uint32 v; byte c[4]; } one = { 0x03020100 };
-            return one.c[0] == 0;
+            return one.c[0] == byte(0);
             //const union { uint32 u; byte c[4]; } one = { 1 };
             //return one.c[3] == 1;
          }

src/bitstream/DebugInputBitStream.cpp
@@ -23,7 +23,7 @@ DebugInputBitStream::DebugInputBitStream(InputBitStream& ibs) THROW : _delegate(
     _idx = 0;
     _mark = false;
     _hexa = false;
-    _current = 0;
+    _current = byte(0);
 }
 
 DebugInputBitStream::DebugInputBitStream(InputBitStream& ibs, ostream& os) THROW : _delegate(ibs), _out(os), _width(80)
@@ -31,7 +31,7 @@ DebugInputBitStream::DebugInputBitStream(InputBitStream& ibs, ostream& os) THROW
     _idx = 0;
     _mark = false;
     _hexa = false;
-    _current = 0;
+    _current = byte(0);
 }
 
 DebugInputBitStream::DebugInputBitStream(InputBitStream& ibs, ostream& os, int width) THROW : _delegate(ibs), _out(os)
@@ -46,7 +46,7 @@ DebugInputBitStream::DebugInputBitStream(InputBitStream& ibs, ostream& os, int w
     _idx = 0;
     _mark = false;
     _hexa = false;
-    _current = 0;
+    _current = byte(0);
 }
 
 DebugInputBitStream::~DebugInputBitStream()
@@ -59,7 +59,7 @@ int DebugInputBitStream::readBit() THROW
 {
     int res = _delegate.readBit();
     _current <<= 1;
-    _current |= res;
+    _current |= byte(res);
     _out << ((res & 1) == 1 ? "1" : "0");
     _idx++;
 
@@ -99,7 +99,7 @@ uint64 DebugInputBitStream::readBits(uint count) THROW
         int bit = (res >> (count - i)) & 1;
         _idx++;
         _current <<= 1;
-        _current |= bit;
+        _current |= byte(bit);
         _out << ((bit == 1) ? "1" : "0");
 
         if ((_mark == true) && (i == count))
@@ -137,10 +137,10 @@ uint DebugInputBitStream::readBits(byte bits[], uint count) THROW
 
     for (uint i = 0; i < (count >> 3); i++) {
         for (int j = 7; j >= 0; j--) {
-            int bit = (bits[i] >> j) & 1;
+            int bit = int(bits[i] >> j) & 1;
             _idx++;
             _current <<= 1;
-            _current |= bit;
+            _current |= byte(bit);
             _out << ((bit == 1) ? "1" : "0");
 
             if ((_mark == true) && (j == int(count)))
@@ -175,7 +175,7 @@ uint DebugInputBitStream::readBits(byte bits[], uint count) THROW
 
 void DebugInputBitStream::printByte(byte b)
 {
-    int val = b & 0xFF;
+    int val = int(b) & 0xFF;
 
     if (val < 10)
         _out << " [00" << val << "] ";

src/bitstream/DebugOutputBitStream.cpp
@@ -22,7 +22,7 @@ DebugOutputBitStream::DebugOutputBitStream(OutputBitStream& obs) THROW : _delega
 {
     _mark = false;
     _hexa = false;
-    _current = 0;
+    _current = byte(0);
     _idx = 0;
 }
 
@@ -30,7 +30,7 @@ DebugOutputBitStream::DebugOutputBitStream(OutputBitStream& obs, OutputStream& o
 {
     _mark = false;
     _hexa = false;
-    _current = 0;
+    _current = byte(0);
     _idx = 0;
 }
 
@@ -45,7 +45,7 @@ DebugOutputBitStream::DebugOutputBitStream(OutputBitStream& obs, OutputStream& o
     _width = width;
     _mark = false;
     _hexa = false;
-    _current = 0;
+    _current = byte(0);
     _idx = 0;
 }
 
@@ -59,7 +59,7 @@ void DebugOutputBitStream::writeBit(int bit) THROW
     bit &= 1;
     _out << ((bit == 1) ? "1" : "0");
     _current <<= 1;
-    _current |= bit;
+    _current |= byte(bit);
     _idx++;
 
     if (_mark == true)
@@ -97,7 +97,7 @@ int DebugOutputBitStream::writeBits(uint64 bits, uint count) THROW
     for (int i = 1; i <= res; i++) {
         uint64 bit = (bits >> (res - i)) & 1;
         _current <<= 1;
-        _current |= bit;
+        _current |= byte(bit);
         _idx++;
         _out << ((bit == 1) ? "1" : "0");
 
@@ -137,9 +137,9 @@ uint DebugOutputBitStream::writeBits(byte bits[], uint count) THROW
 
     for (int i = 0; i < end; i++) {
         for (int j = 7; j >=0 ; j--) {
-           uint64 bit = (bits[i] >> j) & 1;
+           uint64 bit = uint64(bits[i] >> j) & 1;
            _current <<= 1;
-           _current |= bit;
+           _current |= byte(bit);
            _idx++;
            _out << ((bit == 1) ? "1" : "0");
 
@@ -175,7 +175,7 @@ uint DebugOutputBitStream::writeBits(byte bits[], uint count) THROW
 
 void DebugOutputBitStream::printByte(byte b)
 {
-    int val = b & 0xFF;
+    int val = int(b) & 0xFF;
 
     if (val < 10)
         _out << " [00" << val << "] ";

src/bitstream/DefaultInputBitStream.hpp
@@ -112,7 +112,7 @@ namespace kanzi
            uint64 val = 0;
 
            while (_position <= _maxPosition) {
-               val |= ((uint64(_buffer[_position++] & 0xFF)) << shift);
+               val |= ((uint64(_buffer[_position++] & byte(0xFF))) << shift);
                shift -= 8;
            }
 

src/entropy/EntropyCodecFactory.hpp
@@ -40,16 +40,16 @@ namespace kanzi {
 
    class EntropyCodecFactory {
    public:
-       static const byte NONE_TYPE = 0; // No compression
-       static const byte HUFFMAN_TYPE = 1; // Huffman
-       static const byte FPAQ_TYPE = 2; // Fast PAQ (order 0)
-       static const byte PAQ_TYPE = 3; // Obsolete
-       static const byte RANGE_TYPE = 4; // Range
-       static const byte ANS0_TYPE = 5; // Asymmetric Numerical System order 0
-       static const byte CM_TYPE = 6; // Context Model
-       static const byte TPAQ_TYPE = 7; // Tangelo PAQ
-       static const byte ANS1_TYPE = 8; // Asymmetric Numerical System order 1
-       static const byte TPAQX_TYPE = 9; // Tangelo PAQ Extra
+       static const short NONE_TYPE = 0; // No compression
+       static const short HUFFMAN_TYPE = 1; // Huffman
+       static const short FPAQ_TYPE = 2; // Fast PAQ (order 0)
+       static const short PAQ_TYPE = 3; // Obsolete
+       static const short RANGE_TYPE = 4; // Range
+       static const short ANS0_TYPE = 5; // Asymmetric Numerical System order 0
+       static const short CM_TYPE = 6; // Context Model
+       static const short TPAQ_TYPE = 7; // Tangelo PAQ
+       static const short ANS1_TYPE = 8; // Asymmetric Numerical System order 1
+       static const short TPAQX_TYPE = 9; // Tangelo PAQ Extra
 
        static EntropyDecoder* newDecoder(InputBitStream& ibs, map<string, string>& ctx, short entropyType) THROW;
 

src/entropy/ExpGolombDecoder.hpp
@@ -49,7 +49,7 @@ namespace kanzi
    inline byte ExpGolombDecoder::decodeByte()
    {
        if (_bitstream.readBit() == 1)
-           return 0;
+           return byte(0);
 
        int log2 = 1;
 
@@ -58,8 +58,8 @@ namespace kanzi
 
        if (_signed == true) {
            // Decode signed: read value + sign
-           byte res = byte(_bitstream.readBits(log2 + 1));
-           const byte sgn = res & 1;
+           int res = int(_bitstream.readBits(log2 + 1));
+           const int sgn = res & 1;
            res = (res >> 1) + (1 << log2) - 1;
            return byte((res - sgn) ^ -sgn); // res or -res
        }

src/entropy/ExpGolombEncoder.hpp
@@ -87,13 +87,13 @@ namespace kanzi
 
    inline void ExpGolombEncoder::encodeByte(byte val)
    {
-       if (val == 0) {
+       if (val == byte(0)) {
            // shortcut when input is 0
            _bitstream.writeBit(1);
            return;
        }
 
-       const int emit = CACHE[_signed][val&0xFF];
+       const int emit = CACHE[_signed][uint8(val)];
        _bitstream.writeBits(emit&0x1FF, emit>>9);
    }
 }

commit 1e1282518c176fc2991d5192066e559a38ad6171

Fix regression introduced by commit c075607

src/transform/BWTS.cpp
@@ -145,7 +145,7 @@ int BWTS::moveLyndonWordHead(int sa[], int isa[], byte data[], int count, int st
         if ((k == size) && (rank < isa[nextStart]))
             break;
 
-        if ((k < size) && (nextStart < count) && ((data[start + k] & byte(0xFF)) < (data[nextStart] & byte(0xFF))))
+        if ((k < size) && (nextStart < count) && (int(data[start + k] & 0xFF)) < int(data[nextStart] & 0xFF))
             break;
 
         sa[rank] = nextStart0;