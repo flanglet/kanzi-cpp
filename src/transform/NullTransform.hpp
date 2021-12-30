5d8b0d2b5fa7527bf6872ec3eac355978d99cf3a
Frederic Langlet <flanglet@gmail.com>
2021-12-30 14:53:33 -0800

    Simplify

--- src/transform/NullTransform.hpp
+++ src/transform/NullTransform.hpp
@@ -60,13 +60,7 @@ namespace kanzi
        if (output._index + length > output._length)
            return false;
 
-       if (&input._array[0] == &output._array[0]) {
-           if (input._index != output._index)
-               memmove(&output._array[output._index], &input._array[input._index], length);
-       } else {
-           memcpy(&output._array[output._index], &input._array[input._index], length);
-       }
-
+       memcpy(&output._array[output._index], &input._array[input._index], length);
        input._index += length;
        output._index += length;
        return true;
