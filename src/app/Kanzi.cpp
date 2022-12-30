74eece7f2f3144904223c5681c4edde30a78073b
Frederic Langlet <flanglet@gmail.com>
2022-12-30 13:56:06 +0100

    Fix logical expression

--- src/app/Kanzi.cpp
+++ src/app/Kanzi.cpp
@@ -693,7 +693,7 @@ int processCommandLine(int argc, const char* argv[], map<string, string>& map)
     map["verbose"] = strVerbose;
     map["mode"] = mode;
 
-    if ((mode == "c") || (strLevel != "-1"))
+    if ((mode == "c") && (strLevel != "-1"))
         map["level"] = strLevel;
 
     if (strOverwrite == STR_TRUE)
