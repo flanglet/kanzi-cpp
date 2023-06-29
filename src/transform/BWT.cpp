87a0e8cc1d2538a7808ddec668f494fa95c16d5a
Frederic Langlet <flanglet@gmail.com>
2023-06-29 17:51:37 +0200

    Speed optimization

--- src/transform/BWT.cpp
+++ src/transform/BWT.cpp
@@ -210,32 +210,40 @@ bool BWT::inverseMergeTPSI(SliceArray<byte>& input, SliceArray<byte>& output, in
         int t5 = getPrimaryIndex(5) - 1;
         int t6 = getPrimaryIndex(6) - 1;
         int t7 = getPrimaryIndex(7) - 1;
+        byte* d0 = &dst[ckSize * 0];
+        byte* d1 = &dst[ckSize * 1];
+        byte* d2 = &dst[ckSize * 2];
+        byte* d3 = &dst[ckSize * 3];
+        byte* d4 = &dst[ckSize * 4];
+        byte* d5 = &dst[ckSize * 5];
+        byte* d6 = &dst[ckSize * 6];
+        byte* d7 = &dst[ckSize * 7];
         int n = 0;
 
         while (true) {
             const int ptr0 = _buffer[t0];
-            dst[n] = byte(ptr0);
+            d0[n] = byte(ptr0);
             t0 = ptr0 >> 8;
             const int ptr1 = _buffer[t1];
-            dst[n + ckSize * 1] = byte(ptr1);
+            d1[n] = byte(ptr1);
             t1 = ptr1 >> 8;
             const int ptr2 = _buffer[t2];
-            dst[n + ckSize * 2] = byte(ptr2);
+            d2[n] = byte(ptr2);
             t2 = ptr2 >> 8;
             const int ptr3 = _buffer[t3];
-            dst[n + ckSize * 3] = byte(ptr3);
+            d3[n] = byte(ptr3);
             t3 = ptr3 >> 8;
             const int ptr4 = _buffer[t4];
-            dst[n + ckSize * 4] = byte(ptr4);
+            d4[n] = byte(ptr4);
             t4 = ptr4 >> 8;
             const int ptr5 = _buffer[t5];
-            dst[n + ckSize * 5] = byte(ptr5);
+            d5[n] = byte(ptr5);
             t5 = ptr5 >> 8;
             const int ptr6 = _buffer[t6];
-            dst[n + ckSize * 6] = byte(ptr6);
+            d6[n] = byte(ptr6);
             t6 = ptr6 >> 8;
             const int ptr7 = _buffer[t7];
-            dst[n + ckSize * 7] = byte(ptr7);
+            d7[n] = byte(ptr7);
             t7 = ptr7 >> 8;
             n++;
 
