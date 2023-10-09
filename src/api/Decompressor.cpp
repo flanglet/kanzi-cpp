b8a5c68a3c5e9bd7fb2ea38e4e93b528677c18b3
flanglet <flanglet@gmail.com>
2023-10-09 09:01:23 +0200

    Split API code into compression and decompression

--- src/api/Decompressor.cpp
+++ src/api/Decompressor.cpp
@@ -13,13 +13,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "libapi.hpp"
+#include "Decompressor.hpp"
 #include "../types.hpp"
 #include "../Error.hpp"
 #include "../io/CompressedInputStream.hpp"
-#include "../io/CompressedOutputStream.hpp"
 #include "../transform/TransformFactory.hpp"
-#include "../entropy/EntropyEncoderFactory.hpp"
 
 #include <errno.h>
 
@@ -39,29 +37,6 @@ using namespace std;
 using namespace kanzi;
 
 namespace kanzi {
-   // Utility classes to map C FILEs to C++ streams
-   class ofstreambuf : public streambuf
-   {
-     public:
-        ofstreambuf (int fd) : _fd(fd) { }
-
-     private:
-        int _fd; 
-
-        virtual int_type overflow(int_type c) {
-            if (c == EOF)
-               return EOF;
-
-            char d = char(c);
-            return (WRITE(_fd, &d, 1) == 1) ? c : EOF;
-        }
-
-        virtual streamsize xsputn(const char* s, streamsize sz) {
-            return WRITE(_fd, s, sz);
-        }
-    };
-
-
    class ifstreambuf : public streambuf {
      public:
        ifstreambuf(int fd) : _fd(fd) {
@@ -89,19 +64,6 @@ namespace kanzi {
        }
     };
 
-
-    class FileOutputStream FINAL : public ostream
-    {
-       private:
-          ofstreambuf _buf;
-
-       public:
-          FileOutputStream(int fd) : ostream(nullptr), _buf(fd) {
-              rdbuf(&_buf);
-          }
-    };
-
-
     class FileInputStream FINAL : public istream
     {
        private:
@@ -114,117 +76,6 @@ namespace kanzi {
     };
 }
 
-
-// Create internal cContext and CompressedOutputStream
-int CDECL initCompressor(struct cData* pData, FILE* dst, struct cContext** pCtx)
-{
-    if ((pData == nullptr) || (pCtx == nullptr) || (dst == nullptr))
-        return Error::ERR_INVALID_PARAM;
-
-    try {
-        int mLen;
-
-        // Process params
-        string transform = TransformFactory<byte>::getName(TransformFactory<byte>::getType(pData->transform));
-        mLen = min(int(transform.length()), 63);
-        strncpy(pData->transform, transform.data(), mLen);
-        pData->transform[mLen + 1] = 0;
-        string entropy = EntropyEncoderFactory::getName(EntropyEncoderFactory::getType(pData->entropy));
-        mLen = min(int(entropy.length()), 15);
-        strncpy(pData->entropy, entropy.data(), mLen);
-        pData->entropy[mLen + 1] = 0;
-        pData->blockSize = (pData->blockSize + 15) & -16;
-        *pCtx = nullptr;
-        const int fd = FILENO(dst);
-
-        if (fd == -1)
-           return Error::ERR_CREATE_COMPRESSOR;
-
-        // Create compression stream and update context
-        FileOutputStream* fos = new FileOutputStream(fd);
-        cContext* cctx = new cContext();
-        cctx->pCos = new CompressedOutputStream(*fos, pData->entropy, pData->transform, pData->blockSize, bool(pData->checksum & 1), pData->jobs);
-        cctx->blockSize = pData->blockSize;
-        cctx->fos = fos;
-        *pCtx = cctx;
-    }
-    catch (exception&) {
-        return Error::ERR_CREATE_COMPRESSOR;
-    }
-
-    return 0;
-}
-
-int CDECL compress(struct cContext* pCtx, const BYTE* src, int* inSize, int* outSize)
-{
-    *outSize = 0;
-    int res = 0;
-
-    if ((pCtx == nullptr) || (*inSize > int(pCtx->blockSize))) {
-        *inSize = 0;
-        return Error::ERR_INVALID_PARAM;
-    }
-
-    CompressedOutputStream* pCos = (CompressedOutputStream*)pCtx->pCos;
-
-    if (pCos == nullptr)
-        return Error::ERR_INVALID_PARAM;
-
-    try {
-        const uint64 w = pCos->getWritten();
-        pCos->write((const char*)src, streamsize(*inSize));
-        res = pCos->good() ? 0 : Error::ERR_WRITE_FILE;
-        *outSize = int(pCos->getWritten() - w);
-    }
-    catch (exception&) {
-        *inSize = 0; // revisit: could be more accurate
-        return Error::ERR_UNKNOWN;
-    }
-
-    return res;
-}
-
-// Cleanup allocated internal data structures
-int CDECL disposeCompressor(struct cContext* pCtx, int* outSize)
-{
-	 *outSize = 0;
-
-    if (pCtx == nullptr)
-        return Error::ERR_INVALID_PARAM;
-
-    CompressedOutputStream* pCos = (CompressedOutputStream*)pCtx->pCos;
-
-    try {
-        if (pCos != nullptr) {
-            const uint64 w = pCos->getWritten();
-            pCos->close();
-            *outSize = int(pCos->getWritten() - w);
-		  }
-    }
-    catch (exception&) {
-        return Error::ERR_UNKNOWN;
-    }
-
-    try {
-        if (pCos != nullptr)
-            delete pCos;
-
-        if (pCtx->fos != nullptr)
-            delete (FileOutputStream*)pCtx->fos;
-
-        pCtx->fos = nullptr;
-        delete pCtx;
-    }
-    catch (exception&) {
-        if (pCtx->fos != nullptr)
-            delete (FileOutputStream*)pCtx->fos;
-
-        delete pCtx;
-        return Error::ERR_UNKNOWN;
-    }
-
-    return 0;
-}
 
 // Create internal dContext and CompressedInputStream
 int CDECL initDecompressor(struct dData* pData, FILE* src, struct dContext** pCtx)
@@ -268,6 +119,7 @@ int CDECL decompress(struct dContext* pCtx, BYTE* dst, int* inSize, int* outSize
     }
 
     CompressedInputStream* pCis = (CompressedInputStream*)pCtx->pCis;
+    *outSize = 0;
 
     if (pCis == nullptr)
         return Error::ERR_INVALID_PARAM;
@@ -280,7 +132,6 @@ int CDECL decompress(struct dContext* pCtx, BYTE* dst, int* inSize, int* outSize
         *outSize = int(pCis->gcount());
     }
     catch (exception&) {
-        *outSize = 0; // revisit: could be more accurate
         return Error::ERR_UNKNOWN;
     }
 
--- src/api/Decompressor.hpp
+++ src/api/Decompressor.hpp
@@ -32,63 +32,6 @@ limitations under the License.
    typedef unsigned char BYTE;
 
    /**
-    *  Compression parameters
-    */
-   struct cData {
-	   char transform[64];      /* name of transforms  [None|PACK|BWT|BWTS|LZ|LZX|LZP|ROLZ|ROLZX]
-                                                      [RLT|ZRLT|MTFT|RANK|SRT|TEXT|MM|EXE|UTF] */
-	   char entropy[16];        /* name of entropy codec [None|Huffman|ANS0|ANS1|Range|FPAQ|TPAQ|TPAQX|CM] */
-	   unsigned int blockSize;  /* size of block in bytes */
-	   unsigned int jobs;       /* max number of concurrent tasks */
-	   int checksum;            /* bool to indicate use of block checksum */
-   };
-
-   /**
-    *  Compression context: encapsulates compressor state (opaque: could change in future versions)
-    */
-   struct cContext {
-	   void* pCos;
-	   unsigned int blockSize;
-	   void* fos;
-   };
-
-
-    /**
-    *  Initialize the compressor internal states.
-    *
-    *  @param cParam [IN] - the compression parameters
-    *  @param dst [IN] - the destination stream of compressed data
-    *  @param ctx [IN|OUT] - pointer to the compression context created by the call
-    *
-    *  @return 0 in case of success
-    */
-   int CDECL initCompressor(struct cData* cParam, FILE* dst, struct cContext** ctx);
-
-    /**
-    *  Compress a block of data. The compressor must have been initialized.
-    *
-    *  @param ctx [IN] - the compression context created during initialization
-    *  @param src [IN] - the source block of data to compress
-    *  @param inSize [IN|OUT] - the size of the source block to compress.
-                                Updated to reflect the number bytes written to the destination.
-    *  @param outSize [OUT] - the size of the compressed data
-    *
-    *  @return 0 in case of success
-    */
-   int CDECL compress(struct cContext* ctx, const BYTE* src, int* inSize, int* outSize);
-
-   /**
-    *  Dispose the compressor and cleanup memory resources.
-    *
-    *  @param ctx [IN] - the compression context created during initialization
-    *  @param outSize [IN|OUT] - the number of bytes written to the destination
-    *                            (the compressor may flush internal data)
-    *
-    *  @return 0 in case of success
-    */
-   int CDECL disposeCompressor(struct cContext* ctx, int* outSize);
-
-   /**
     *  Decompression parameters
     */
    struct dData {
