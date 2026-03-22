3878c32bd14b9e9bf9d95e681761c5b6a6b4bc63
Frederic Langlet <flanglet@gmail.com>
2026-03-21 21:57:27 -0400

    Minor logic fix

--- src/app/BlockDecompressor.cpp
+++ src/app/BlockDecompressor.cpp
@@ -261,7 +261,7 @@ int BlockDecompressor::decompress(uint64& inputSize)
             else if ((inputIsDir == true) && (specialOutput == false)) {
                 oName = formattedOutName + iName.substr(formattedInName.size());
 
-                if ((upperInputName.length() >= 4) && (upperInputName.substr(upperInputName.length() - 4) == ".KNZ"))
+                if ((upperIName.length() >= 4) && (upperIName.substr(upperIName.length() - 4) == ".KNZ"))
                     oName.resize(oName.length() - 4);
                 else
                     oName = oName + ".bak";
